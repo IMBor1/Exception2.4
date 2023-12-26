d95df83eb43d403db7ca11dfb453baf6897a8de3
IMBor1 <ilacerber1@gmail.com>
2023-12-26 22:31:27 +0300

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import exception.WrongLoginException;
+import exception.WrongPasswordException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            Person.infoPerson("etw", "138", "138");
+        } catch (WrongLoginException e) {
+            System.out.println("Не правильно ввели логин." +
+                    " Используйте только латинские буквы, цифры и знак подчеркивания");
+        } catch (WrongPasswordException e) {
+            System.out.println("Не правильно ввели пароль." +
+                    " Используйте только латинские буквы, цифры и знак подчеркивания.");
+        }
+
+
     }
+
+
 }
